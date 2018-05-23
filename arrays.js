548a70ae8351967a11a271d55d1620f4bf179e35
Kyle <kyleverret@yahoo.com>
2018-05-23 15:25:51 +0000

    automatically backed up by learn

--- arrays.js
+++ arrays.js
@@ -28,7 +28,7 @@ function destructivelyRemoveElementFromBeginningOfArray(arr){
 }
 
 function removeElementFromBeginningOfArray(arr){
-  return [...arr.slice(0,1)];
+  return [...arr.slice(0)];
 } 
 
 function destructivelyRemoveElementFromEndOfArray(arr){
