05981f8f084724911247e709aa496497daba03c9
Kyle <kyleverret@yahoo.com>
2018-05-23 15:23:42 +0000

    automatically backed up by learn

--- arrays.js
+++ arrays.js
@@ -28,7 +28,7 @@ function destructivelyRemoveElementFromBeginningOfArray(arr){
 }
 
 function removeElementFromBeginningOfArray(arr){
-  arr =  arr.slice(0,1);
+  arr.slice(0,1);
   return arr;
 } 
 
