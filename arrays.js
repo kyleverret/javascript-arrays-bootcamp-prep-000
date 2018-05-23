4b24c8a0f068b0844d7a22570c2e13ea8eb3475f
Kyle <kyleverret@yahoo.com>
2018-05-23 15:23:10 +0000

    automatically backed up by learn

---
+++ arrays.js
@@ -0,0 +1,40 @@
+var chocolateBars = ['snickers', 'hundred grand', 'kitkat', 'skittles'];
+
+function addElementToBeginningOfArray(arr, newEl){
+  return [newEl, ...arr];
+}
+
+function destructivelyAddElementToBeginningOfArray(arr, newEl){
+  arr.unshift(newEl);
+  return arr;
+}
+
+function addElementToEndOfArray(arr, newEl){
+  return [ ...arr, newEl];
+}
+
+function destructivelyAddElementToEndOfArray(arr, newEl){
+  arr.push(newEl);
+  return arr;
+}
+
+function accessElementInArray(arr, index){
+  return arr[index];
+}
+
+function destructivelyRemoveElementFromBeginningOfArray(arr){
+  return arr.shift();
+}
+
+function removeElementFromBeginningOfArray(arr){
+  arr =  arr.slice(0,1);
+  return arr;
+} 
+
+function destructivelyRemoveElementFromEndOfArray(arr){
+  return arr.pop();
+}
+
+function removeElementFromEndOfArray(arr){
+  return arr.slice(0, arr.length-1);
+}
